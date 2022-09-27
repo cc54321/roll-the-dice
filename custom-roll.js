3e2a825eaec5a61b9ae52e0f129fd752d1468cf8
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-09-27 09:29:28 -0400

    Create custom-roll.js

---
+++ custom-roll.js
@@ -0,0 +1,12 @@
+const promt = require('promt-sync')({sigint: true});
+
+let player = prompt("roll the dice:")
+
+rollthedice = math.floor(math.random ()*1); //0 or 1
+
+if(rollthedice === 0){
+    console.log("heads");
+    else if(rollthedice === 2){
+        ("tails");
+    }
+}
