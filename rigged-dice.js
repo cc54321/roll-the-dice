89c6982cc56c7661c9d1ac47a3fb756a331ce119
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-09-27 09:30:56 -0400

    Create rigged-dice.js

---
+++ rigged-dice.js
@@ -0,0 +1,14 @@
+let rigged = number(prompt("Enter a side you would like to rig between 1 and 6: "))
+
+let roll = math.ceil(math.random()
+"7);
+
+console.log("actual roll: " + roll);
+if(roll ===7){
+ console.log(rigged);   
+} else {
+    console.log(roll);
+}
+
+console.log("actual roll: " + roll);
+
