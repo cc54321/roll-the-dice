dfcef8b4baab1008af4b162c973ed8cf8e4a0504
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 23:38:08 -0400

    Update rigged-dice.js

--- rigged-dice.js
+++ rigged-dice.js
@@ -1,14 +1,36 @@
-let Rigged = number(prompt("Enter a side you would like to rig between 1 and 6: "))
+/*const promt = require('promt-sync')({sigint: true});
 
-let Roll = math.ceil(math.random()
-"7);
+/*
 
-console.log("actual Roll: " + Roll);
-if(Roll ===7){
- console.log(Rigged);   
+we're workong with a 6 sided die
+
+riged side 6
+
+1 = 1/6
+2 = 1/6
+3 = 1/6
+4 = 1/6
+5 = 1/6
+6 = 1/6
+
+roll a 7 sided die 
+
+1-6 die
+with 2 6s 7 is being treated as another 6
+*/
+const prompt = require('prompt-sync')({sigint: true});
+
+let rigged = Number(prompt("Enter a side you would like to rig between 1 and 6: "))
+
+let roll = Math.ceil(Math.random()
+*7);
+console.log("actual roll: " + roll);
+if(roll === 7){
+ console.log(rigged);   
 } else {
-    console.log(Roll);
+    console.log(roll);
 }
 
-console.log("actual Roll: " + Roll);
+
+
 
